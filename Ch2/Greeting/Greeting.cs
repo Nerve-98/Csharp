ee1c840c5d76c699702b7ec8a5283aad349fa4e8
Nerve-98 <rudtjq0331@gmail.com>
2020-12-25 10:55:20 +0900

    simple greeting

---
+++ Ch2/Greeting/Greeting.cs
@@ -0,0 +1,15 @@
+using System;
+using static System.Console;
+
+namespace Greet
+{
+    class Greeting
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hi, everyone?");
+            WriteLine("greeting!");
+
+        }
+    }
+}
