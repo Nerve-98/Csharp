83cbaf199f4272955a9d07021448fe27c848ed38
Nerve-98 <rudtjq0331@gmail.com>
2021-01-03 21:01:51 +0900

    generic and constraint

--- Ch11/ConstrainsOnTypeParameters.cs
+++ Ch11/ConstrainsOnTypeParameters.cs
@@ -2,7 +2,7 @@ using System;
 
 class StructArray<T> where T : struct
 {
-    public T[] Array { get; set }
+    public T[] Array { get; set; }
     public StructArray(int size)
     {
         Array = new T[size];
@@ -20,3 +20,66 @@ class RefArray<T> where T : class
     }
 
 }
+
+class Base { }
+class Derived : Base { }
+class BaseArray<U> where U : Base
+{
+    public U[] Array { get; set; }
+    public BaseArray(int size)
+    {
+        Array = new U[size];
+    }
+
+    public void CopyArray<T>(T[] Source) where T : U
+    {
+        Source.CopyTo(Array, 0);
+    }
+}
+
+class MainApp
+{
+    public static T CreateInstance<T>() where T : new()
+    {
+        return new T();
+
+    }
+
+    static void Main(string[] args)
+    {
+        StructArray<int> a = new StructArray<int>(3); // T = int, where T : struct
+
+        a.Array[0] = 0;
+        a.Array[1] = 1;
+        a.Array[2] = 2;
+
+        RefArray<StructArray<double>> b = new RefArray<StructArray<double>>(3); // where T : class
+        b.Array[0] = new StructArray<double>(5);
+        b.Array[1] = new StructArray<double>(10);
+        b.Array[2] = new StructArray<double>(100);
+
+        BaseArray<Base> c = new BaseArray<Base>(3);
+        c.Array[0] = new Base();        // where U : Base
+        c.Array[1] = new Derived();     // Derived : Base
+        c.Array[2] = CreateInstance<Base>();
+
+        BaseArray<Derived> d = new BaseArray<Derived>(3);
+        d.Array[0] = new Derived();
+        d.Array[1] = CreateInstance<Derived>(); // same to new Derived();
+        d.Array[2] = CreateInstance<Derived>();
+
+        BaseArray<Base> e = new BaseArray<Base>(3);
+        e.CopyArray<Derived>(d.Array);
+
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(c);
+        Console.WriteLine(d);
+        Console.WriteLine(e);
+
+
+    }
+
+}
+
