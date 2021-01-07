a736a02057262975d13ec461fa8dd76140782fd7
Nerve-98 <rudtjq0331@gmail.com>
2021-01-07 23:30:39 +0900

    lambda first

---
+++ Ch14/SimpleLambda.cs
@@ -0,0 +1,18 @@
+using System;
+
+class MainApp
+{
+    delegate int Calculate(int a, int b);
+
+
+    static void Main(string[] agrs)
+    {
+        Calculate calc = (a, b) => a + b;
+
+        Console.WriteLine($"3 + 4 : {calc(3, 4)}");
+
+
+    }
+
+
+}
