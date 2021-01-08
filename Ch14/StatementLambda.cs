3685b0dc8d2defd85df9d20dc2adc62066e86c19
Nerve-98 <rudtjq0331@gmail.com>
2021-01-08 22:50:39 +0900

    statment lambda

---
+++ Ch14/StatementLambda.cs
@@ -0,0 +1,27 @@
+using System;
+
+class MainApp
+{
+    delegate string Concatenate(string[] args);
+
+
+    static void Main(string[] args)
+    {
+        Concatenate concat =
+            (arr) =>
+            {
+                string result = "";
+                foreach (string s in arr)
+                    result += s;
+
+                return result;
+
+            };
+
+        Console.WriteLine(concat(args));
+    }
+
+
+
+
+}
