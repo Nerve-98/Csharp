07226f6ceb0125ff475ddfc13f8813f6a22415d6
Nerve-98 <rudtjq0331@gmail.com>
2021-01-25 23:21:17 +0900

    thread and abort

---
+++ Ch19/AbortingThread.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Threading;
+
+class SideTask
+{
+    int count;
+
+    public SideTask(int count)
+    {
+        this.count = count;
+    }
+
+    public void KeepAlive()
+    {
+        try
+        {
+
+            while(count > 0)
+            {
+                Console.WriteLine($"{count--} left");
+                Thread.Sleep(10);
+               
+
+            }
+            Console.WriteLine("Count : 0");
+
+        }
+        catch(ThreadAbortException e)
+        {
+            Console.WriteLine(e);
+            Thread.ResetAbort();
+        }
+        finally
+        {
+            Console.WriteLine("Clear resource...");
+        }
+
+
+
+    }
+}
+
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        SideTask task = new SideTask(1000);
+        Thread t1 = new Thread(new ThreadStart(task.KeepAlive));
+        t1.IsBackground = false;
+
+
+        t1.Start();
+        Thread.Sleep(1000);
+        Console.WriteLine("Aborting thread...");
+        t1.Abort();
+
+        Console.WriteLine("Waiting until thread stops...");
+        t1.Join();
+
+        Console.WriteLine("Finished");
+
+
+    }
+}
